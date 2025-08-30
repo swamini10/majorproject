e7c8949c7e0685fa326ba1b669fb3dfe275984db
swamini10 <swaminibhandare10@gmail.com>
2025-08-30 15:14:46 +0530

    connected to db

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 const express =require("express");
 const app =express();
-const mongooes =require("mongoose");
+const mongoose =require("mongoose");
+
+const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust"
+
+main()
+    .then(() => { 
+        console.log("connected to db");
+    })
+    .catch((err) => {console.log(err);
+    });
+
+async function main() {
+    await mongoose.connect(MONGO_URL);
+}
 
 // start server we use  // nodemon app.js
 
