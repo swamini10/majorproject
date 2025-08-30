da549aef36d3ec4f670b307604f5f1ca11bca3b4
swamini10 <swaminibhandare10@gmail.com>
2025-08-30 14:49:10 +0530

    check api

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ const express =require("express");
 const app =express();
 const mongooes =require("mongoose");
 
-app.listen(8080, () => {    // nodemon app.js
+// start server we use  // nodemon app.js
+
+app.get("/", (req,res)   => {  // for check visit http://localhost:8080/
+    res.send("Hi, i am root ");
+});
+
+app.listen(8080, () => {
     console.log("server is running at port 8080")
 })
