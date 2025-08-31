df3b6cf088779a887c438d2786d364ada52a15dd
swamini10 <swaminibhandare10@gmail.com>
2025-08-31 06:34:55 +0530

    allListings

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express =require("express");
 const app =express();
 const mongoose =require("mongoose");
 const Listing = require("./models/listing.js");
+const path = require("path");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust"
 
@@ -9,31 +10,41 @@ main()
     .then(() => { 
         console.log("connected to db");
     })
-    .catch((err) => {console.log(err);
+    .catch((err) => {
+        console.log(err);
     });
 
 async function main() {
     await mongoose.connect(MONGO_URL);
 }
 
-// start server we use  // nodemon app.js
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
+// start server we use  // nodemon app.js
 app.get("/", (req,res)   => {  // for check visit http://localhost:8080/
     res.send("Hi, i am root ");
 });
 
-app.get("/listings", async (req,res)   => {  // for check visit http://localhost:8080/listings
-    let sampleListing = new Listing({
-        title: "my New Villa",
-        description: "By the beach",
-        price: 1200,
-        location: "Calangute, Goa",
-        country: "India"
-    });
-    await sampleListing.save();
-    console.log("Sample was Saved ");
-    res.send("Successful testing");
+app.get("/listings", async (req,res)   => { 
+    const allListings = await Listing.find({});
+    res.render("listings/index.ejs", { allListings });
 });
+
+
+
+// app.get("/listings", async (req,res)   => {  // for check visit http://localhost:8080/listings
+//     let sampleListing = new Listing({
+//         title: "my New Villa",
+//         description: "By the beach",
+//         price: 1200,
+//         location: "Calangute, Goa",
+//         country: "India"
+//     });
+//     await sampleListing.save();
+//     console.log("Sample was Saved ");
+//     res.send("Successful testing");
+// });
 
 app.listen(8080, () => {
     console.log("server is running at port 8080")
